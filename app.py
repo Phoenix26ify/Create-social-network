f7240c213f500a39703a72fc4d3936b9a90ca045
Phoenix26ify <43580116+Phoenix26ify@users.noreply.github.com>
2019-06-05 15:43:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+# This code creates a UI for the user to post their posts on the social media
+# Author : Shreya Mitra
+# Created : Shreya Mitra
+# Date : 05.06.2019
+# Version : 1.1
+
 from flask import Flask, render_template, request
 from flask_cors import CORS
 from model import create_post, get_posts
